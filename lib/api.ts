36ab03ec2ffec7a1152f52d64b5f48dace306d75
Antonio RAMANANDRAIBE <102760708+antonioramana@users.noreply.github.com>
2026-03-28 08:11:06 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 const API_BASE =
   typeof window === 'undefined'
-    ? process.env.SERVER_API_BASE || 'http://localhost:4000'
+    ? process.env.SERVER_API_BASE || 'http://localhost:4000/api'
     : process.env.NEXT_PUBLIC_API_BASE || '/api';
 
 function getAuthToken(): string | null {
